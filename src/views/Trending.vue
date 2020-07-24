e01f6f1f6eb4d1ebe44832ccb846191f2474ccef
yukinagato017 <jecagustin017@gmail.com>
2020-07-24 21:19:47 +0800

    haha

--- src/views/Trending.vue
+++ src/views/Trending.vue
@@ -33,7 +33,6 @@
             ></vue-fontawesome>
             Share</span
           >
-          <iframe src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fedyaryo.netlify.app%2Ftrending&layout=button_count&size=small&appId=2901853709885468&width=77&height=20" width="77" height="20" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
         </div>
         <div class="title">
           <h2>{{ newsfeeds.title }}</h2>
