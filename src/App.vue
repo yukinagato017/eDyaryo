b5f027a95f835d7d00539139e31605d17b58edc1
yukinagato017 <jecagustin017@gmail.com>
2020-07-24 14:57:51 +0800

    hato

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@
             <div class="link">
               <ShareNetwork
                 network="facebook"
-    url="https://news.vuejs.org/issues/180"
+    url="https://edyaryo.netlify.app"
     title="Say hi to Vite! A brand new, extremely fast development setup for Vue."
     description="This week, I’d like to introduce you to 'Vite', which means 'Fast'. It’s a brand new development setup created by Evan You."
     quote="The hot reload is so fast it\'s near instant. - Evan You"
